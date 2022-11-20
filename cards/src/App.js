35fe7d884fe0c446d6fafce21221506c5fa9d925
s-araqayyum <saraqayyum268@gmail.com>
2022-11-20 23:24:50 +0500

    Creating dummy application

---
+++ cards/src/App.js
@@ -0,0 +1,95 @@
+import * as React from 'react';
+
+import card from './card1.jpg'
+import card2 from './card2.jpg'
+
+import AspectRatio from '@mui/joy/AspectRatio';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import IconButton from '@mui/joy/IconButton';
+import Typography from '@mui/joy/Typography';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Card variant="outlined" sx={{ width: 320 }}>
+        <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>The Sun Tarot</Typography>
+        <Typography level="body2">April 24 to May 02, 2021</Typography>
+        <IconButton
+          aria-label="Sun Tarot"
+          variant="plain"
+          color="neutral"
+          size="sm"
+          sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+        >
+        </IconButton>
+        <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
+          <img
+            src={card}
+            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
+        <Box sx={{ display: 'flex' }}>
+          <div>
+            <Typography level="body3">Total price:</Typography>
+            <Typography fontSize="lg" fontWeight="lg">
+              $2,900
+            </Typography>
+          </div>
+          <Button
+            variant="solid"
+            size="sm"
+            color="primary"
+            aria-label="Explore Bahamas Islands"
+            sx={{ ml: 'auto', fontWeight: 600 }}
+          >
+            Explore
+          </Button>
+        </Box>
+      </Card>
+
+      <br></br>
+
+      <Card variant="outlined" sx={{ width: 320 }}>
+        <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>The Moon Tarot</Typography>
+        <Typography level="body2">April 24 to May 02, 2021</Typography>
+        <IconButton
+          aria-label="Sun Tarot"
+          variant="plain"
+          color="neutral"
+          size="sm"
+          sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+        >
+        </IconButton>
+        <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
+          <img
+            src={card2}
+            srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
+        <Box sx={{ display: 'flex' }}>
+          <div>
+            <Typography level="body3">Total price:</Typography>
+            <Typography fontSize="lg" fontWeight="lg">
+              $2,900
+            </Typography>
+          </div>
+          <Button
+            variant="solid"
+            size="sm"
+            color="primary"
+            aria-label="Explore Bahamas Islands"
+            sx={{ ml: 'auto', fontWeight: 600 }}
+          >
+            Explore
+          </Button>
+        </Box>
+      </Card>
+    </div>
+  );
+}
